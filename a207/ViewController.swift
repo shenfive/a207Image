45e74598b0ba75c69083b49ad4755b530ffc08c6
Danny Shen <shenfive@gmail.com>
2020-08-15 12:41:10 +0800

    cell

--- a207/ViewController.swift
+++ a207/ViewController.swift
@@ -8,17 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var myImageView: UIImageView!
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+
+    
+    var imageArray = [UIImage]()
+    @IBOutlet weak var myCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let myImagePath = Bundle.main.path(forResource: "pic1", ofType: "jpeg"){
-            myImageView.image = UIImage.init(contentsOfFile: myImagePath)
-        }
+        imageArray = [
+            UIImage(named: "pic1")!,
+            UIImage(named: "pic2")!,
+            UIImage(named: "pic3")!,
+            UIImage(named: "pic4")!,
+            UIImage(named: "pic5")!
+        ]
+        myCollectionView.delegate = self
+        myCollectionView.dataSource = self
+        print("lunch")
+
 
     }
 
 
+    
+    //MARK:CollectView Data Source & Delegate
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
 }
 
