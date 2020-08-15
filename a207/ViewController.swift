a5e2d93b12d7e9ec438ac63a6b402a33ef8889f1
Danny Shen <shenfive@gmail.com>
2020-08-15 10:34:03 +0800

    init

--- a207/ViewController.swift
+++ a207/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let myImagePath = Bundle.main.path(forResource: "pic1", ofType: "jpeg"){
+            myImageView.image = UIImage.init(contentsOfFile: myImagePath)
+        }
+
     }
 
 
